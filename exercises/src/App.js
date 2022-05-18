50cdd6bad085ea6fc4e8fb784adbfa064d0164bf
HNDuy Nguyen <duynguyen11062001@gmail.com>
2022-05-18 13:41:58 +0300

    Exercise 8 & 9

--- exercises/src/App.js
+++ exercises/src/App.js
@@ -9,21 +9,25 @@ import Ex4 from './components/Ex4';
 import Ex5 from './components/Ex5';
 import Ex6 from './components/Ex6';
 import Ex7 from './components/Ex7';
+import Ex8 from './components/Ex8';
+import Ex9 from './components/Ex9';
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <nav className="navbar">
+        <nav className="navbar" style={{display: 'flex', flexDirection: 'row', justifyContent: 'center'}}>
           <ul>
-            <Link to="/" style={{marginRight: 20}}>Home</Link>
-            <Link to="/ex1" style={{marginRight: 20}}>Exercise 1</Link>
-            <Link to="/ex2" style={{marginRight: 20}}>Exercise 2</Link>
-            <Link to="/ex3" style={{marginRight: 20}}>Exercise 3</Link>
-            <Link to="/ex4" style={{marginRight: 20}}>Exercise 4</Link>
-            <Link to="/ex5" style={{marginRight: 20}}>Exercise 5</Link>
-            <Link to="/ex6" style={{marginRight: 20}}>Exercise 6</Link>
-            <Link to="/ex7" style={{marginRight: 20}}>Exercise 7</Link>
+            <Link to="/" style={{marginRight: 30}}>Home</Link>
+            <Link to="/ex1" style={{marginRight: 30}}>Exercise 1</Link>
+            <Link to="/ex2" style={{marginRight: 30}}>Exercise 2</Link>
+            <Link to="/ex3" style={{marginRight: 30}}>Exercise 3</Link>
+            <Link to="/ex4" style={{marginRight: 30}}>Exercise 4</Link>
+            <Link to="/ex5" style={{marginRight: 30}}>Exercise 5</Link>
+            <Link to="/ex6" style={{marginRight: 30}}>Exercise 6</Link>
+            <Link to="/ex7" style={{marginRight: 30}}>Exercise 7</Link>
+            <Link to="/ex8" style={{marginRight: 30}}>Exercise 8</Link>
+            <Link to="/ex9" style={{marginRight: 30}}>Exercise 9</Link>
           </ul>
         </nav>
         <Routes>
@@ -35,6 +39,8 @@ function App() {
           <Route path="/ex5" element={<Ex5/>}></Route>
           <Route path="/ex6" element={<Ex6/>}></Route>
           <Route path="/ex7" element={<Ex7/>}></Route>
+          <Route path="/ex8" element={<Ex8/>}></Route>
+          <Route path="/ex9" element={<Ex9/>}></Route>
         </Routes>
       </div>
     </Router>
---
+++ exercises/src/components/Ex9.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Ex9 = () => {
+    return (
+        <div className="Ex9" style={{ textAlign: 'center' }}>
+            <button style={{
+                marginTop: 50, width: 300, height: 100,
+                borderRadius: 8, border: 'none', background: 'red', color: 'white', cursor: 'pointer'
+            }}>Fetch Random</button>
+        </div>
+    )
+}
+
+export default Ex9
